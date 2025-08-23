9e2fdab348a48ba6fdbdba0be50c3e583f3f4c32
hannamalana98 <hannamalana98@gmail.com>
2025-08-24 00:29:38 +0800

    feature: advertise.jsx - uploaded the video to vimeo for storage

--- client/src/components/Advertise/Advertise.jsx
+++ client/src/components/Advertise/Advertise.jsx
@@ -5,15 +5,16 @@ import "../Home/home.css";
 import "./advertise.css";
 
 
+
 export const Advertise = () => {
     return (
-        <div className=" side-padding flex flex-col h-screen py-15 ">
+        <div className=" side-padding flex flex-col h-auto py-15 ">
 
         <div className="">
-            <span className=" flex justify-center text-black font fontMedium">Your key to the perfect home</span>
+            <span className=" flex justify-center text-black font text-[32px] lg:fontMedium">Your key to the perfect home</span>
         </div>
 
-        <div className="flex flex-col lg:flex-row flex-6 justify-between py-5">
+        <div className="flex flex-col lg:flex-row flex-6 justify-between py-5 ">
             <div className="flex flex-col w-full lg:w-1/2 justify-start items-start">
                 {/* <h1 className="text-black">left</h1> */}
                 {advertise.map(({id, heading, context}) => (
@@ -25,15 +26,23 @@ export const Advertise = () => {
                 ))}
             </div>
 
-            <div className="flex w-full lg:w-1/2 justify-center items-center">
-                {/* <h1 className="text-black">right</h1> */}
-                <video className="rounded-lg shadow-2xl" autoPlay muted loop playsInline>
+            <div className="flex lg:w-1/2 justify-center items-center overflow-hidden py-5 lg:py-0">
+                {/* <video className="rounded-lg shadow-2xl" autoPlay muted loop playsInline>
                     <source
                         src="/videos/houseDemo.mp4"
                         type="video/mp4"
                     />
                     Your browser does not support the video tag.
-                </video>
+                </video> */}
+                <iframe src="https://player.vimeo.com/video/1112554069?autoplay=1&muted=1&loop=1&background=1&dnt=1&playsinline=1"
+                    className="w-full h-full"
+                    allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share" 
+                    referrerPolicy="strict-origin-when-cross-origin" 
+                    title="houseDemo">
+                </iframe>
+
+
+                
             </div>
             
 
