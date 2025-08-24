1976f04a0e15af9ef9eabf9006a6321f72b2ba67
hannamalana98 <hannamalana98@gmail.com>
2025-08-24 23:22:18 +0800

    docs of hoe IntersectionObserver works

--- client/src/components/Advertise/useInView.jsx
+++ client/src/components/Advertise/useInView.jsx
@@ -25,4 +25,6 @@ const useInView = (options) => {
 
 export default useInView;
 
-// intersectionObserver ginagamit to
+// this handles the useEffect or effect of animation ease in and out when scrolled down and when the target element is visible in the screen 
+// by using thresh0ld 0 or  1 
+// 1 being the 100% of the element and 0 is like 0.01px of the element is visible run the effect
