7614b0602dafbbd4e5af476e96137f42c9bd43c3
hannamalana98 <hannamalana98@gmail.com>
2025-08-24 19:00:04 +0800

    feature: finished the Advertise.jsx section

--- client/src/components/Advertise/Advertise.jsx
+++ client/src/components/Advertise/Advertise.jsx
@@ -1,8 +1,10 @@
 
 import { advertise } from "./advertise.data.jsx";
+import './useInView.jsx'
 
 import "../Home/home.css";
 import "./advertise.css";
+import useInView from "./useInView.jsx";
 
 
 
@@ -16,19 +18,19 @@ export const Advertise = () => {
 
         <div className="flex flex-col lg:flex-row flex-6 justify-between py-5">             
             <div className="flex flex-col w-full lg:w-1/2 justify-start items-start">                 
-                {advertise.map(({id, heading, context}, index) => (                     
-                    <div 
-                        key={id} 
-                        className="text-black py-5 px-8 opacity-0 animate-fade-in"
-                        style={{
-                            animationDelay: `${index * 0.2}s`,
-                            animationFillMode: 'forwards'
-                        }}
-                    >                         
-                        <span className="flex py-5 text-3xl">{heading}</span>                         
-                        <span className="text-xl">{context}</span>                      
-                    </div>                 
-                ))}             
+                {advertise.map(({id, heading, context}, index) => {
+                    const [ref, isInView] = useInView({threshold: 1});
+
+                    return (
+                        <div key={id} ref={ref} className={`text-black py-5 px-8 transform transition-all duration-700 ease-out ${isInView ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"}`}
+                        style={{transitionDelay: `${index * 0.2}s`}}>
+                            <span className="flex py-5 text-3xl">{heading}</span>
+                            <span className="text-xl">{context}</span>
+
+                        </div>
+
+                    )
+                })}        
             </div>
 
             <div className="flex lg:w-1/2 h-auto justify-center items-center py-5 lg:py-0">
@@ -39,7 +41,7 @@ export const Advertise = () => {
                     />
                     Your browser does not support the video tag.
                 </video> */}
-                <iframe src="https://player.vimeo.com/video/1112554069?autoplay=1&muted=1&loop=1&background=1&dnt=1&playsinline=1"
+                <iframe src="https://player.vimeo.com/video/1112554069?autoplay=1&loop=1&playsinline=1"
                     className="lg:w-full lg:h-full w-2xl h-96" 
                     // width="1920" height="1080"
                     allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share" 
---
+++ client/src/components/Advertise/useInView.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useRef, useState } from "react"
+
+const useInView = (options) => {
+    const ref = useRef(null);
+    const [isInView, setisInView] = useState(false);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(([entry], obs) => {
+            if(entry.isIntersecting){
+                setisInView(true);
+                obs.unobserve(entry.target)
+            }
+        }, options)
+
+        if (ref.current) observer.observe(ref.current)
+
+        return () => {
+            if (ref.current) observer.unobserve(ref.current)
+        };
+    }, [options])
+
+    return [ref, isInView];
+
+}
+
+export default useInView;
